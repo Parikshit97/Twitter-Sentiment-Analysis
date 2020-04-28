fcf26f1f52ee17a4ecef833f6916140577885049
PARIKSHIT NARANG <parikshitnarang@administrators-MacBook-Pro-7.local>
2020-04-29 02:31:01 +0530

    remove apringboot app

--- demo/src/main/java/com/example/demo/Feature.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface Feature {
-    public HashMap<String, HashMap<String,Double>> calculate(ArrayList<Hotels> hotels, String userprofileid) throws IOException;
-}
